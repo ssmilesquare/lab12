31b47fb4334f85481a7e2b9725382753fec2c4ef
ssmilesquare <ssmilesquare@gmail.com>
2021-01-19 16:22:30 +0700

    lab12_1 completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -17,3 +17,23 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0, Min = A[0], Max = A[0], stdDev = 0, multiply = 1, sumHarmo;
+    for(int i = 0; i < N; ++i){
+        sum += A[i];
+        multiply *= A[i];
+        sumHarmo += 1.0/A[i];
+        if(A[i] > Max) Max = A[i];
+        if(A[i] < Min) Min = A[i];
+    }
+    B[0] = sum/N;
+    for(int i = 0; i < N; ++i){
+        stdDev += (pow(A[i],2)-pow(B[0],2));
+    }
+    B[1] = sqrt(1.0/N*stdDev);
+    B[2] = pow(multiply,1.0/N);
+    B[3] = N / sumHarmo;
+    B[4] = Max;
+    B[5] = Min;
+}
